570f3ec2e3b6f4807180d2d4ff4f4962fcd5e4dd
Brady Snuggs <dronebug360@gmail.com>
2018-11-09 13:10:16 -1000

    added server.

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express'),
+      app = express(),
+      {SERVER_PORT} = process.env
+app.use(express.static(`${__dirname}/../dist`))
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Magic is happening on ${SERVER_PORT}`)
+})
