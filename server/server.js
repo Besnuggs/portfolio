500df0727ce1bedbb5f3ed716a8f6f20b44a567e
Brady Snuggs <dronebug360@gmail.com>
2021-03-13 14:09:33 -0700

    ran prettier and still updating about.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const express = require('express'),
-      app = express(),
-      {SERVER_PORT} = process.env
-app.use(express.static(`${__dirname}/../dist`))
+require("dotenv").config();
+const express = require("express"),
+  app = express(),
+  { SERVER_PORT } = process.env;
+app.use(express.static(`${__dirname}/../dist`));
 
 app.listen(SERVER_PORT, () => {
-    console.log(`Magic is happening on ${SERVER_PORT}`)
-})
+  console.log(`Magic is happening on ${SERVER_PORT}`);
+});
