a76340e10c4435e3b81d642db02c4f28c2269f84
Brady Snuggs <bradyesnuggs@gmail.com>
2022-12-27 14:39:36 +0000

    Moved socket-training mini project into portfolio.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
 require("dotenv").config();
 const express = require("express"),
   app = express(),
+  socket = require('socket.io'),
   { SERVER_PORT } = process.env;
-app.use(express.static(`${__dirname}/../dist`));
 
-app.listen(SERVER_PORT, () => {
+const server = app.listen(SERVER_PORT, () => {
   console.log(`Magic is happening on ${SERVER_PORT}`);
 });
+
+app.use(express.static(`${__dirname}/../dist`));
+
+// Socket Connection and Server
+const io = socket(server);
+io.on('connection', (socket) => {
+        console.log('made socket connection', socket.id);
+        // Handle chat event
+        socket.on('chat', function(data){
+                // console.log(data);
+                io.emit('chat', data);
+        });
+
+        // Handle typing event
+        socket.on('typing', function(data){
+                socket.broadcast.emit('typing', data);
+        });
+            
+        
+})
